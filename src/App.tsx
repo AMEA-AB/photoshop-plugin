8cd1b8b73fcb6986fe6b5db2175acefcd49a2d01
Albin Medoc <stamline203@gmail.com>
2023-06-17 16:08:43 +0200

    Split code

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import Spectrum from 'react-uxp-spectrum';
 
 import { storage } from 'uxp';
-import { app, core, constants } from 'photoshop';
+import { core } from 'photoshop';
 
 import './App.css';
 import AmeaService from '@services/amea-service';
-import PhotoshopService from '@services/photoshop-service';
-import DownloadService from '@services/download-service';
 import type { Document } from 'photoshop/dom/Document';
 import GenerateInputs from '@components/GenerateInputs';
 
@@ -19,51 +17,6 @@ export default function App() {
     const [outputFolder, setOutputFolder] = React.useState<storage.Folder | undefined>(undefined);
     const [templatesFolder, setTemplatesFolder] = React.useState<storage.Folder | undefined>(undefined);
     const [outputType, setOutputType] = React.useState<OutputType>('png');
-
-    const populateDocumentFromRow = async (row: DataRow) => {
-        for (const columnName of Object.keys(row)) {
-            const layer = app.activeDocument.layers.getByName(columnName);
-            if (!layer) continue;
-            if (layer.kind === constants.LayerKind.TEXT) {
-                await core.executeAsModal(() => PhotoshopService.setText(layer, row[columnName]), { commandName: `Change text for '${columnName}'` });
-            }
-            else if (layer.kind === constants.LayerKind.SMARTOBJECT) {
-                const fileURL = row[columnName];
-                const filenameRegex = /.*\/(.*)/g;
-                const filename = filenameRegex.exec(fileURL)[1];
-                const file = await DownloadService.getFileFromWeb(fileURL, filename);
-                core.executeAsModal(() => PhotoshopService.setImage(layer, file), { commandName: `Change image for '${columnName}'` });
-            }
-        }
-    }
-
-    const addEffectsToDocumentFromRow = async (row: DataRow) => {
-        // Invert colors
-        const invert = row['Invert'] === 'true';
-        for (const layer of app.activeDocument.layers) {
-            await core.executeAsModal(() => PhotoshopService.invertLayer(layer, invert), { commandName: 'Inverting colors' });
-        }
-
-        // Mirror document
-        const mirror = row['Mirror'] === 'true';
-        if (mirror) {
-            await core.executeAsModal(() => PhotoshopService.mirrorDocument(app.activeDocument), { commandName: 'Mirroring document' });
-        }
-    }
-
-    const exportDocument = async (outputFolder: storage.Folder, filename: string, outputType: OutputType = 'png') => {
-        const outputFile = await outputFolder.createFile(filename, { overwrite: true });
-        await core.executeAsModal(() => app.activeDocument.saveAs[outputType](outputFile as unknown as File), { commandName: 'Exporting file' })
-    }
-
-    const generateImageFromRow = async (row: DataRow, templatesFolder: storage.Folder, outputFolder: storage.Folder, filename: string, outputType: OutputType = 'png') => {
-        const templateFile = await templatesFolder.getEntry(row.Template) as storage.File;
-        await PhotoshopService.openPhotoshopFile(templateFile);
-        await populateDocumentFromRow(row);
-        await addEffectsToDocumentFromRow(row);
-        await exportDocument(outputFolder, filename, outputType);
-        await PhotoshopService.closeDocument();
-    }
 
     const handleGenerate = async () => {
         const templates = rows.map((row) => row.Template).filter((value, index, self) => self.indexOf(value) === index);
@@ -79,7 +32,7 @@ export default function App() {
         let proccessedRows = 0;
         for (const row of AmeaService.mapFilenames(rows)) {
             try {
-                await generateImageFromRow(row, templatesFolder ?? tempFolder, outputFolder, row.filename, outputType);
+                await AmeaService.generateImageFromRow(row, templatesFolder ?? tempFolder, outputFolder, row.filename, outputType);
                 proccessedRows += 1;
             } catch (error) {
                 console.error(error);
--- src/services/amea-service.ts
+++ src/services/amea-service.ts
@@ -1,5 +1,13 @@
+import { storage } from 'uxp';
+import { app, core, constants } from 'photoshop';
+
 import DownloadService from '@services/download-service';
+import PhotoshopService from '@services/photoshop-service';
 import { replaceAllInString } from '@helpers';
+
+import type { Document } from 'photoshop/dom/Document';
+
+type OutputType = keyof Document['saveAs'];
 
 class AmeaService {
   public static async downloadTemplateFile(templateName: string) {
@@ -21,13 +29,58 @@ class AmeaService {
     return rows.map((row) => {
       let filename = this.generateFilename(row);
       let i = 2;
-      while(filenames.includes(filename)) {
+      while (filenames.includes(filename)) {
         filename = `${this.generateFilename(row)} ${i}`;
         i++;
       }
       filenames.push(filename);
-      return {...row, filename};
+      return { ...row, filename };
     });
+  }
+
+  private static async populateDocumentFromRow(row: DataRow) {
+    for (const columnName of Object.keys(row)) {
+      const layer = app.activeDocument.layers.getByName(columnName);
+      if (!layer) continue;
+      if (layer.kind === constants.LayerKind.TEXT) {
+        await core.executeAsModal(() => PhotoshopService.setText(layer, row[columnName]), { commandName: `Change text for '${columnName}'` });
+      }
+      else if (layer.kind === constants.LayerKind.SMARTOBJECT) {
+        const fileURL = row[columnName];
+        const filenameRegex = /.*\/(.*)/g;
+        const filename = filenameRegex.exec(fileURL)[1];
+        const file = await DownloadService.getFileFromWeb(fileURL, filename);
+        core.executeAsModal(() => PhotoshopService.setImage(layer, file), { commandName: `Change image for '${columnName}'` });
+      }
+    }
+  }
+
+  private static async addEffectsToDocumentFromRow(row: DataRow) {
+    // Invert colors
+    const invert = row['Invert'] === 'true';
+    for (const layer of app.activeDocument.layers) {
+      await core.executeAsModal(() => PhotoshopService.invertLayer(layer, invert), { commandName: 'Inverting colors' });
+    }
+
+    // Mirror document
+    const mirror = row['Mirror'] === 'true';
+    if (mirror) {
+      await core.executeAsModal(() => PhotoshopService.mirrorDocument(app.activeDocument), { commandName: 'Mirroring document' });
+    }
+  }
+
+  private static async exportDocument(outputFolder: storage.Folder, filename: string, outputType: OutputType = 'png') {
+    const outputFile = await outputFolder.createFile(filename, { overwrite: true });
+    await core.executeAsModal(() => app.activeDocument.saveAs[outputType](outputFile as unknown as File), { commandName: 'Exporting file' })
+  }
+
+  public static async generateImageFromRow(row: DataRow, templatesFolder: storage.Folder, outputFolder: storage.Folder, filename: string, outputType: OutputType = 'png') {
+    const templateFile = await templatesFolder.getEntry(row.Template) as storage.File;
+    await PhotoshopService.openPhotoshopFile(templateFile);
+    await this.populateDocumentFromRow(row);
+    await this.addEffectsToDocumentFromRow(row);
+    await this.exportDocument(outputFolder, filename, outputType);
+    await PhotoshopService.closeDocument();
   }
 }
 
