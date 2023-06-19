46727a6dc0ba1a2869c4dbb97ae4f8c139ce2a11
Albin Medoc <stamline203@gmail.com>
2023-06-19 07:30:33 +0200

    Only validate .env file when developing

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = function (_env, argv) {
         },
         plugins: [
             new Dotenv({
-                safe: true,
+                safe: isDevelopment,
             }),
             new CleanWebpackPlugin(),
             new CopyPlugin({
